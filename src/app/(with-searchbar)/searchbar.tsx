7136b1b972e18a50c3158b0e3d6a25e41ef54e8e
jmeno1011 <jwhltn8282@gmail.com>
2024-09-25 22:55:06 +0900

    day 10 homework

--- src/app/(with-searchbar)/searchbar.tsx
+++ src/app/(with-searchbar)/searchbar.tsx
@@ -1,16 +1,38 @@
-// "use client"
-// import { useState } from 'react'
+"use client"
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react'
 
 export default function Searchbar() {
-  // const [search, setSearch] = useState("")
-  // const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearch(e.target.value);
-  // }
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const q = searchParams.get('q');
+
+  const [search, setSearch] = useState("")
+
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSubmit();
+    }
+  }
+
+  useEffect(() => {
+    setSearch(q || "")
+  }, [q])
+
   return (
     <div>
-      {/* <input value={search} onChange={onChangeSearch} type="text" />
-      <button>검색</button> */}
-      Searchbar Layout
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} type="text" />
+      <button onClick={onSubmit}>검색</button>
     </div>
   )
 }
